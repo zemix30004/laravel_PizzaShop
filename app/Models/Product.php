c08f4d510d395ef2ff6edf790d05b5a31e881458
zemix30004 <eugenezm@gmail.com>
2021-08-10 20:25:24 +0300

    laravel_PizzaShop
    laravel_PizzaShop

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+use App\Models\Product;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+}
